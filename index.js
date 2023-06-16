2ffbbb3bff42ede9ad9742325f4c90cae74cd14a
unknown <jtsterlin@yahoo.com>
2023-06-15 23:17:41 -0400

    updated functionality, adding photos and formatting is next

--- index.js
+++ index.js
@@ -22,30 +22,6 @@ function addUser() {
   alert('User added successfully.');
 }
 
-function updateBalance() {
-  const amountInput = document.getElementById('amount');
-  const amount = parseFloat(amountInput.value);
-  
-  const user = document.getElementById('user').value.trim();
-  if (user === '') {
-    alert('Please enter a user name.');
-    return;
-  }
-  
-  if (!(user in balances)) {
-    alert('User does not exist. Please add the user first.');
-    return;
-  }
-  
-  balances[user] += amount;
-  
-  amountInput.value = '';
-  amountInput.focus();
-  
-  localStorage.setItem('balances', JSON.stringify(balances));
-  updateBalanceList();
-}
-
 function incrementBalance(user) {
   if (!(user in balances)) {
     alert('User does not exist.');
@@ -68,38 +44,55 @@ function decrementBalance(user) {
   updateBalanceList();
 }
 
+function clearUser(user) {
+  if (confirm('Are you sure you want to clear this user?')) {
+    delete balances[user];
+    localStorage.setItem('balances', JSON.stringify(balances));
+    updateBalanceList();
+    alert('User cleared.');
+  }
+}
+
 function updateBalanceList() {
   const userBalancesElement = document.querySelector('#userBalances tbody');
   userBalancesElement.innerHTML = '';
-  
-  for (const user in balances) {
+
+  const sortedUsers = Object.keys(balances).sort();
+
+  for (const user of sortedUsers) {
     const row = document.createElement('tr');
-    
+
     const userCell = document.createElement('td');
     userCell.textContent = user;
-    
+
     const balanceCell = document.createElement('td');
     balanceCell.textContent = '$' + Math.round(balances[user]);
-    
+
     const actionsCell = document.createElement('td');
-    
+
     const incrementButton = document.createElement('button');
     incrementButton.textContent = '+';
-    incrementButton.className = 'balance-button';
+    incrementButton.className = 'balance-button increment-button';
     incrementButton.onclick = function() { incrementBalance(user); };
-    
+
     const decrementButton = document.createElement('button');
     decrementButton.textContent = '-';
-    decrementButton.className = 'balance-button';
+    decrementButton.className = 'balance-button decrement-button';
     decrementButton.onclick = function() { decrementBalance(user); };
-    
+
+    const clearButton = document.createElement('button');
+    clearButton.textContent = 'Clear User';
+    clearButton.className = 'balance-button clear-button';
+    clearButton.onclick = function() { clearUser(user); };
+
     actionsCell.appendChild(incrementButton);
     actionsCell.appendChild(decrementButton);
-    
+    actionsCell.appendChild(clearButton);
+
     row.appendChild(userCell);
     row.appendChild(balanceCell);
     row.appendChild(actionsCell);
-    
+
     userBalancesElement.appendChild(row);
   }
 }
